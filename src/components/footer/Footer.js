458b5c6591a550c30bb425b6476a9515b1095c8d
Clay Gao <claygao@Alinade-MacBook-Air.local>
2019-12-10 02:17:10 +0800

    add navgationbar and react-router

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { Segment, Menu, Icon } from 'semantic-ui-react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Footer = () => (
+    <div></div>
+)
+
+export default Footer
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Segment, Menu, Icon } from 'semantic-ui-react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Tab = ({to, name, active, onClick, icon}) => (
+  <Menu.Item
+    as={Link}
+    to={to}
+    name={name}
+    active={active === name}
+    onClick={onClick}
+  >
+    <Icon name={icon} />
+    {name}
+  </Menu.Item>
+)
+
+const Nav = props => {
+  
+  const [activeItem, setActiveItem] = useState('home')
+  const handleItemClick = (e, {name}) => {
+    setActiveItem(name)
+  }
+  
+  return (
+      <Segment inverted>
+        <Menu inverted secondary>
+          <Tab to="/" name="Dashboard" icon="dashboard" active={activeItem} onClick={handleItemClick}/>
+          <Tab to="/weather" name="Weather" icon="cloud" active={activeItem} onClick={handleItemClick}/>
+          <Tab to="/todolist" name="Todolist" icon="list" active={activeItem} onClick={handleItemClick} />
+        </Menu>
+      </Segment>
+  )
+}
+
+export default Nav
